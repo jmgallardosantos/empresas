50ca9980a07defa2b6e6265e37603a858201a633
Ricardo Pérez <ricpelo@gmail.com>
2024-10-08 20:51:41 +0200

    Crea departamentos.php

---
+++ departamentos.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Departamentos</title>
+</head>
+<body>
+    <?php
+    $pdo = new PDO('pgsql:host=localhost;dbname=datos', 'datos', 'datos');
+    $stmt = $pdo->query('SELECT *
+                           FROM departamentos
+                       ORDER BY codigo');
+    ?>
+    <table border="1">
+        <thead>
+            <th>Código</th>
+            <th>Denominación</th>
+        </thead>
+        <tbody>
+            <?php foreach ($stmt as $fila): ?>
+                <tr>
+                    <td><?= $fila['codigo'] ?></td>
+                    <td><?= $fila['denominacion'] ?></td>
+                </tr>
+            <?php endforeach ?>
+        </tbody>
+    </table>
+    <!-- // $filas = $stmt->fetchAll();
+    // foreach ($filas as $fila) {
+    //     var_dump($fila);
+    // }
+    // while ($fila = $stmt->fetch()) {
+    //     var_dump($fila);
+    // } -->
+</body>
+</html>
